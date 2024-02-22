d8217463fde5569d3e330f07280ba577c9a39180
PankajSahi <pankajkumar01072003nwd@gmail.com>
2024-02-22 10:14:09 +0530

    Add New Files

--- DOM/dom1.js
+++ DOM/dom1.js
@@ -1,24 +1,26 @@
 
 const parent = document.querySelector('.parent')
-const btn = document.querySelector('.btn')
+// const btn = document.querySelector('.btn')
 
 console.log(parent);
 // console.log(parent.children);
 // console.log(parent.children[1].innerHTML);
 
 
-// for(let i = 0; i< parent.children.length; i++){
-//     const element = parent.children[i].innerHTML
-//     console.log(element);
-// }
+for(let i = 0; i< parent.children.length; i++){
+    const element = parent.children[i].innerHTML
+    console.log(element);
+}
 
-parent.children[2].style.color = 'red';
-// console.log(parent.firstElementChild);
+// parent.children[2].style.color = 'red';
+// console.log(parent.secondElementChild.innerHTML);
 // console.log(parent.lastElementChild);
 
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 const dayOne = document.querySelector('.day')
-// console.log(dayOne.innerHTML);
+// console.log(dayOne);
+// console.log(dayOne.parentElement);
+console.log(dayOne.nextElementSibling);
 // console.log(dayOne.parentElement.innerHTML)
 // console.log(dayOne.nextElementSibling.innerHTML)
 
